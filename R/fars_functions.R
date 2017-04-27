403ab82b9dfe5a84252a20d66e1b30d1294568c4
Huntley <Bart.Huntley@DPaW.wa.gov.au>
2017-04-27 14:00:20 +0800

    se for summarize years

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -188,9 +188,9 @@ fars_read_years <- function(years) {
 fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>% 
-    dplyr::group_by(year, MONTH) %>% 
-    dplyr::summarize(n = n()) %>%
-    tidyr::spread(year, n)
+    dplyr::group_by_(~MONTH, ~year) %>% 
+    dplyr::summarize_(.dots = dots) %>%
+    tidyr::spread_("year", "sum(MONTH)")
 }
 
 
